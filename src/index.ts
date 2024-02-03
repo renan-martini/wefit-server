8f991de592b1216c74c83422e2cd59eddbbe1722
Renan Martini <renan@Mac-mini-de-Renan.local>
2024-02-03 18:40:42 -0300

    doc(profile): created docs for the profile api using swagger

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,9 @@ import express from "express";
 import AppDataSource from "./data-source";
 import { appRoutes } from "./routes";
 import { errorHandler } from "./middlewares/error.middleware";
+import swaggerUI from "swagger-ui-express";
+import swaggerFile from "./doc/swagger_output.json";
+import swaggerAutogen from "swagger-autogen";
 
 const app = express();
 app.use(express.json());
@@ -14,6 +17,8 @@ app.get("/ping", (_, res) => {
 });
 
 appRoutes(app);
+
+app.use("/doc", swaggerUI.serve, swaggerUI.setup(swaggerFile));
 
 app.use(errorHandler);
 
